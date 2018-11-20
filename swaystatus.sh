6a563ab444aea10d9018257e20a03823b7413ccb
Brian <btoye.dev@outlook.com>
2018-11-20 23:51:34 +0000

    Nearly working display of volume

--- swaystatus.sh
+++ swaystatus.sh
@@ -3,4 +3,6 @@
 # Battery status
 BAT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|percentage" | sed "s/:[ \t]*/: /" | xargs)
 
-printf "%s\n" "BAT: $BAT"
+VOL=$(pactl list sinks | grep -o -E -m1 "(\/.*?\/)")
+
+printf "%s\n" "BAT: $BAT VOL: $VOL"
