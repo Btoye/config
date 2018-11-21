0545ce81669474b117ce9af0694a1354f5ae9f03
Brian <btoye.dev@outlook.com>
2018-11-21 20:06:20 +0000

    use code rom internet rather than spending days

--- swaystatus.sh
+++ swaystatus.sh
@@ -3,6 +3,8 @@
 # Battery status
 BAT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|percentage" | sed "s/:[ \t]*/: /" | xargs)
 
-VOL=$(pactl list sinks | grep -o -E -m1 "(\/.*?\/)")
+# System volume taken from web. Spent too long trying to grep the volume
+VOL=$(pactl list sinks | grep '^[[:space:]]Volume:' | \
+    head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
 
-printf "%s\n" "BAT: $BAT VOL: $VOL"
+printf "%s\n" "BAT: $BAT VOL: $VOL%"
