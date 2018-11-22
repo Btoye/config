3dc0850b095a381442c4548c85efb8f5ca74982a
Brian <btoye.dev@outlook.com>
2018-11-22 21:06:38 +0000

    add todos

--- swaystatus.sh
+++ swaystatus.sh
@@ -7,4 +7,6 @@ BAT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|pe
 VOL=$(pactl list sinks | grep '^[[:space:]]Volume:' | \
     head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
 
+# Show muted status
+
 printf "%s\n" "BAT: $BAT VOL: $VOL%"
